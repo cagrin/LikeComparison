a243f09d2326e4731b63c8af6ae2c0cae99ddc0b
Krzysztof Strugiński <cagrin@gmail.com>
2021-09-15 00:30:02 +0200

    DataGrid: server-side data loading and paging

--- Blazorise.DataGrid.Template/Data/WeatherForecastService.cs
+++ Blazorise.DataGrid.Template/Data/WeatherForecastService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,12 +7,30 @@ namespace Blazorise.DataGrid.Template.Data
 {
     public class WeatherForecastService
     {
+        public WeatherForecast[] Data { get; set; }      
+
+        public int TotalItems { get; set; }
+
+        public async Task OnReadDataAsync(DataGridReadDataEventArgs<WeatherForecast> e)
+        {
+            if (_forecasts == null)
+            {
+                var items = await GetForecastAsync(DateTime.Now);
+                _forecasts = items.AsQueryable<WeatherForecast>();
+            }
+       
+            TotalItems = _forecasts.Count();     
+            Data = _forecasts.Skip((e.Page - 1) * e.PageSize).Take(e.PageSize).ToArray();
+        }      
+
+        private IQueryable<WeatherForecast> _forecasts; 
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        private Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -20,6 +39,6 @@ namespace Blazorise.DataGrid.Template.Data
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToArray());
-        }
+        }  
     }
 }
