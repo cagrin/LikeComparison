c18ccbf3bf2102a1ad5337f5d40f7fc8240c2638
Krzysztof Strugiński <cagrin@gmail.com>
2021-09-15 20:24:57 +0200

    DataGrid: server-side multiple column sorting

--- Blazorise.DataGrid.Template/Data/WeatherForecastService.cs
+++ Blazorise.DataGrid.Template/Data/WeatherForecastService.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazorise;
 
 namespace Blazorise.DataGrid.Template.Data
 {
     public class WeatherForecastService
     {
-        public WeatherForecast[] Data { get; set; }      
+        public WeatherForecast[] Data { get; set; }
 
         public int TotalItems { get; set; }
 
@@ -18,12 +19,31 @@ namespace Blazorise.DataGrid.Template.Data
                 var items = await GetForecastAsync(DateTime.Now);
                 _forecasts = items.AsQueryable<WeatherForecast>();
             }
-       
-            TotalItems = _forecasts.Count();     
-            Data = _forecasts.Skip((e.Page - 1) * e.PageSize).Take(e.PageSize).ToArray();
-        }      
 
-        private IQueryable<WeatherForecast> _forecasts; 
+            var query = _forecasts;
+
+            var sortedColumns = e.Columns.Where(f => f.SortIndex >= 0).OrderByDescending(f => f.SortIndex).ToArray();
+            foreach (var column in sortedColumns)
+            {
+                query = (column.Field, column.SortDirection) switch
+                {
+                    (nameof(WeatherForecast.Date), SortDirection.Ascending) => query.OrderBy(f => f.Date),
+                    (nameof(WeatherForecast.Date), SortDirection.Descending) => query.OrderByDescending(f => f.Date),
+                    (nameof(WeatherForecast.TemperatureC), SortDirection.Ascending) => query.OrderBy(f => f.TemperatureC),
+                    (nameof(WeatherForecast.TemperatureC), SortDirection.Descending) => query.OrderByDescending(f => f.TemperatureC),
+                    (nameof(WeatherForecast.TemperatureF), SortDirection.Ascending) => query.OrderBy(f => f.TemperatureF),
+                    (nameof(WeatherForecast.TemperatureF), SortDirection.Descending) => query.OrderByDescending(f => f.TemperatureF),
+                    (nameof(WeatherForecast.Summary), SortDirection.Ascending) => query.OrderBy(f => f.Summary),
+                    (nameof(WeatherForecast.Summary), SortDirection.Descending) => query.OrderByDescending(f => f.Summary),
+                    _ => query
+                };
+            }
+
+            TotalItems = query.Count();
+            Data = query.Skip((e.Page - 1) * e.PageSize).Take(e.PageSize).ToArray();
+        }
+
+        private IQueryable<WeatherForecast> _forecasts;
 
         private static readonly string[] Summaries = new[]
         {
@@ -33,12 +53,12 @@ namespace Blazorise.DataGrid.Template.Data
         private Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(1, 25).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToArray());
-        }  
+        }
     }
 }
