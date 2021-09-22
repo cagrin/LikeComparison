a177845f9a01edfd68a2e51b8fe917ca929cbe27
Krzysztof Strugiński <cagrin@gmail.com>
2021-09-22 23:38:27 +0200

    Tests: Problem with async execution

--- Blazorise.DataGrid.Template.Tests/Extentions/SqlLikeOperatorTests.cs
+++ Blazorise.DataGrid.Template.Tests/Extentions/SqlLikeOperatorTests.cs
@@ -1,12 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Blazorise.DataGrid.Template.Extensions;
-using System;
 using Dapper;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections.Concurrent;
 
 namespace Blazorise.DataGrid.Template.Tests.Extensions
 {
@@ -17,32 +15,29 @@ namespace Blazorise.DataGrid.Template.Tests.Extensions
         public void SqlLikeOperator()
         {
             var cases = GenerateTestCases();
-            int count = cases.Count();
-            Assert.AreEqual(count, 363363);
 
-            Parallel.ForEach(Partitioner.Create(0, count), async (range) =>
+            Assert.AreEqual(cases.Count(), 79860);
+
+            Parallel.ForEach(cases, c =>
             {
-                for (int i = range.Item1; i < range.Item2; i++)
-                {
-                    var c = cases.ElementAt(i);
-                    string matchExpression = c[0].ToString();
-                    string pattern = c[1].ToString();
+                string matchExpression = c[0].ToString();
+                string pattern = c[1].ToString();
 
-                    var expected = await SqlLikeOperatorAsync(matchExpression, pattern);
-                    var actual = StringLike.SqlLikeOperator(matchExpression, pattern);
-                    Assert.AreEqual(actual, expected);
-                }
+                var expected = SqlLikeOperator(matchExpression, pattern);
+                var actual = StringLike.SqlLikeOperator(matchExpression, pattern);
+
+                Assert.AreEqual(actual, expected);
             });
         }
 
-        private async Task<bool> SqlLikeOperatorAsync(string matchExpression, string pattern)
+        private bool SqlLikeOperator(string matchExpression, string pattern)
         {
             const string _connectionString = "Data Source=localhost;Initial Catalog=master;User Id=sa;Password=StrongP@ssw0rd!";
             const string _query = "SELECT CASE WHEN EXISTS(SELECT 1 FROM (SELECT @matchExpression AS matchExpression) Query WHERE Query.matchExpression LIKE @pattern) THEN 1 ELSE 0 END";
 
             using(var connection = new SqlConnection(_connectionString))
             {
-                var result = await connection.ExecuteScalarAsync<bool>(_query, new { matchExpression = matchExpression, pattern = pattern});
+                var result = connection.ExecuteScalar<bool>(_query, new { matchExpression = matchExpression, pattern = pattern});
                 return result;
             }
         }
@@ -73,7 +68,7 @@ namespace Blazorise.DataGrid.Template.Tests.Extensions
                 }
             }
 
-            var patterns = combi.Where(x => x.Contains("%")).ToArray();
+            var patterns = combi.Where(x => x.Contains("%")).Where(x => x.Length < 5).ToArray();
             var matchExpressions = combi.Where(x => !x.Contains("%")).ToArray();
 
             foreach (var pattern in patterns)
