0847e3b42c6a915ef516f7af6218f2142d34b190
Krzysztof Strugiński <cagrin@gmail.com>
2021-10-28 12:39:43 +0200

    Fix SA1201 issues

--- LikeComparison/LikeOptions.cs
+++ LikeComparison/LikeOptions.cs
@@ -2,6 +2,13 @@ using System;
 
 namespace LikeComparison
 {
+    internal enum PatternStyle
+    {
+        VisualBasic = 0,
+
+        TransactSql = 1
+    }
+
     internal class LikeOptions
     {
         public LikeOptions()
@@ -19,12 +26,5 @@ namespace LikeComparison
         public string Invert { get => this.PatternStyle == PatternStyle.VisualBasic ? "!" : "^"; }
 
         public string Digits { get => this.PatternStyle == PatternStyle.VisualBasic ? "#" : "[0-9]"; }
-    }
-
-    internal enum PatternStyle
-    {
-        VisualBasic = 0,
-
-        TransactSql = 1
     }
 }
