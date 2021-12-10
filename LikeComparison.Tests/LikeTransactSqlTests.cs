2acc3edfcabdbd325bc79b7754edac312ca9a8d9
Krzysztof Strugiński <cagrin@gmail.com>
2021-12-10 01:05:03 +0100

    Add WildcardCharactersAsLiteralsTests

--- LikeComparison.Tests/LikeTransactSqlTests.cs
+++ LikeComparison.Tests/LikeTransactSqlTests.cs
@@ -40,6 +40,39 @@ namespace LikeComparison.Tests
             testcontainer?.DisposeAsync().AsTask().Wait();
         }
 
+        [DataTestMethod] // https://docs.microsoft.com/en-us/sql/t-sql/language-elements/like-transact-sql?view=sql-server-ver15
+        /* [DataRow("5[%]", "5%", true)] */
+        [DataRow("5[%]", "5", false)]
+        /* [DataRow("[_]n", "_n", true)] */
+        [DataRow("[_]n", "n", false)]
+        [DataRow("[a-cdf]", "-", false)]
+        [DataRow("[a-cdf]", "a", true)]
+        [DataRow("[a-cdf]", "b", true)]
+        [DataRow("[a-cdf]", "c", true)]
+        [DataRow("[a-cdf]", "d", true)]
+        [DataRow("[a-cdf]", "e", false)]
+        [DataRow("[a-cdf]", "f", true)]
+        [DataRow("[-acdf]", "-", true)]
+        [DataRow("[-acdf]", "a", true)]
+        [DataRow("[-acdf]", "b", false)]
+        [DataRow("[-acdf]", "c", true)]
+        [DataRow("[-acdf]", "d", true)]
+        [DataRow("[-acdf]", "e", false)]
+        [DataRow("[-acdf]", "f", true)]
+        [DataRow("[[]", "[", true)]
+        [DataRow("]", "]", true)]
+        /* [DataRow("abc[_]d%", "abc_d", true)] */
+        /* [DataRow("abc[_]d%", "abc_de", true)] */
+        [DataRow("abc[def]", "abcd", true)]
+        [DataRow("abc[def]", "abce", true)]
+        [DataRow("abc[def]", "abcf", true)]
+        public async Task WildcardCharactersAsLiteralsTests(string pattern, string matchExpression, bool expected)
+        {
+            bool actual = await LikeTransactSqlAssert(matchExpression, pattern).ConfigureAwait(false);
+
+            Assert.AreEqual(expected, actual);
+        }
+
         [DataTestMethod]
         [DataRow("aAB", "%", 26620)]
         [DataRow("/\\", "_%", 9610)]
@@ -60,21 +93,27 @@ namespace LikeComparison.Tests
                 string matchExpression = c[0].ToString();
                 string pattern = c[1].ToString();
 
-                var expected = await LikeTransactSqlOperatorAsync(matchExpression, pattern).ConfigureAwait(false);
-                var regex = LikeTransactSql.LikeRegex(pattern) ?? "<Null>";
-                var message = $"Query:'{matchExpression}' LIKE '{pattern}'. Regex:{regex}";
-
-                try
-                {
-                    var actual = matchExpression.Like(pattern);
-                    Assert.AreEqual(expected, actual, message);
-                }
-                catch (Exception ex)
-                {
-                    Assert.IsTrue(false, message + $". Exception:{ex.Message}.");
-                    throw;
-                }
+                await LikeTransactSqlAssert(matchExpression, pattern).ConfigureAwait(false);
             }).Wait();
+        }
+
+        private static async Task<bool> LikeTransactSqlAssert(string matchExpression, string pattern)
+        {
+            var expected = await LikeTransactSqlOperatorAsync(matchExpression, pattern).ConfigureAwait(false);
+            var regex = LikeTransactSql.LikeRegex(pattern) ?? "<Null>";
+            var message = $"Query:'{matchExpression}' LIKE '{pattern}'. Regex:{regex}";
+
+            try
+            {
+                var actual = matchExpression.Like(pattern);
+                Assert.AreEqual(expected, actual, message);
+                return actual;
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, message + $". Exception:{ex.Message}.");
+                throw;
+            }
         }
 
         private static async Task<bool> LikeTransactSqlOperatorAsync(string matchExpression, string pattern)
