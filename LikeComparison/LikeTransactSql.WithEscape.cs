b315fb659a817d56b41d6ec8e1e78ba68ac0d00d
Krzysztof Strugiński <cagrin@gmail.com>
2021-12-12 00:52:02 +0100

    Add partial LikeTransactSql.WithEscape.cs

---
+++ LikeComparison/LikeTransactSql.WithEscape.cs
@@ -0,0 +1,27 @@
+namespace LikeComparison.TransactSql
+{
+    using System;
+
+    public static partial class LikeTransactSql
+    {
+        public static bool Like(this string matchExpression, string pattern, string escape)
+        {
+            if (escape == null)
+            {
+                throw new ArgumentNullException(nameof(escape), "Value cannot be null.");
+            }
+
+            return LikeString.Like(matchExpression, pattern, LikeOptions);
+        }
+
+        public static string? LikeRegex(string pattern, string escape)
+        {
+            if (escape == null)
+            {
+                throw new ArgumentNullException(nameof(escape), "Value cannot be null.");
+            }
+
+            return LikeString.LikeRegex(pattern, LikeOptions);
+        }
+    }
+}
--- LikeComparison/LikeTransactSql.cs
+++ LikeComparison/LikeTransactSql.cs
@@ -1,9 +1,7 @@
 namespace LikeComparison.TransactSql
 {
-    using System;
-
     // https://docs.microsoft.com/en-us/sql/t-sql/language-elements/like-transact-sql
-    public static class LikeTransactSql
+    public static partial class LikeTransactSql
     {
         private static readonly LikeOptions LikeOptions = new LikeOptions() { PatternStyle = PatternStyle.TransactSql };
 
@@ -12,28 +10,8 @@ namespace LikeComparison.TransactSql
             return LikeString.Like(matchExpression, pattern, LikeOptions);
         }
 
-        public static bool Like(this string matchExpression, string pattern, string escape)
-        {
-            if (escape == null)
-            {
-                throw new ArgumentNullException(nameof(escape), "Value cannot be null.");
-            }
-
-            return LikeString.Like(matchExpression, pattern, LikeOptions);
-        }
-
         public static string? LikeRegex(string pattern)
         {
-            return LikeString.LikeRegex(pattern, LikeOptions);
-        }
-
-        public static string? LikeRegex(string pattern, string escape)
-        {
-            if (escape == null)
-            {
-                throw new ArgumentNullException(nameof(escape), "Value cannot be null.");
-            }
-
             return LikeString.LikeRegex(pattern, LikeOptions);
         }
     }
