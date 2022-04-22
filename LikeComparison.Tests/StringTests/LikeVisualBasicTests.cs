2439637dc25de6a8a1a2ca7502242121753a5ad8
Krzysztof Strugiński <cagrin@gmail.com>
2022-04-22 15:53:31 +0200

    Fix write conflicts in parallel output @testfx

--- LikeComparison.Tests/StringTests/LikeVisualBasicTests.cs
+++ LikeComparison.Tests/StringTests/LikeVisualBasicTests.cs
@@ -1,4 +1,4 @@
-[assembly: Parallelize(Workers = 8, Scope = ExecutionScope.MethodLevel)]
+[assembly: Parallelize(Workers = 8, Scope = ExecutionScope.ClassLevel)]
 
 namespace LikeComparison.StringTests
 {
